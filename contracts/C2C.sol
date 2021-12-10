9a9763908ac084eaaa2d1300f3182fdc203aeb64
antron3000 <fortyonepointsix@gmail.com>
2021-12-10 11:24:25 -0500

    simple C2C token contract

---
+++ contracts/C2C.sol
@@ -0,0 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract C2C is ERC20 {
+    constructor() ERC20("C2C", "C2C") {
+        _mint(msg.sender, 21e27);
+    }
+}
