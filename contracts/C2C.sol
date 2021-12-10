e119a46819e6a3da22ceb4bf7f2b374d998dd404
Antoine De Vuyst <fortyonepointsix@gmail.com>
2021-12-10 11:37:07 -0500

    Update C2C.sol

--- contracts/C2C.sol
+++ contracts/C2C.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract C2C is ERC20 {
     constructor() ERC20("C2C", "C2C") {
-        _mint(msg.sender, 21e27);
+        _mint(msg.sender, 21e27); //21 Billion Supply with 18 decimals
     }
 }
